05f9898233ada289b37491d75f8b3113c9b35fbd
buckapi <buckapi@localhost.localdomain>
2024-03-26 07:39:26 -0500

    employes detail

--- dist/js/contact-data.js
+++ dist/js/contact-data.js
@@ -28,13 +28,10 @@ $(function() {
 	if ($("#datable_1").length > 0) {
 		/*Checkbox Add*/
 		var tdCnt=0;
-		$('table tr').each(function(){
-			$('<span class="form-check mb-0"><input type="checkbox" class="form-check-input check-select" id="chk_sel_'+tdCnt+'"><label class="form-check-label" for="chk_sel_'+tdCnt+'"></label></span>').insertBefore($(this).find("td > .d-flex .contact-star").eq(0));
-			tdCnt++;
-		});
+	
 		var targetDt = $('#datable_1').DataTable({
 			"dom": '<"row"<"col-7 mb-3"<"contact-toolbar-left">><"col-5 mb-3"<"contact-toolbar-right"flip>>><"row"<"col-sm-12"t>><"row"<"col-sm-12 col-md-5"i><"col-sm-12 col-md-7"p>>',
-			"ordering": true,
+			"ordering": false,
 			"columnDefs": [ {
 				"searchable": false,
 				"orderable": false,
@@ -42,7 +39,7 @@ $(function() {
 			} ],
 			"order": [1, 'asc' ],
 			language: { search: "",
-				searchPlaceholder: "Search",
+				searchPlaceholder: "Buscar",
 				"info": "_START_ - _END_ of _TOTAL_",
 				sLengthMenu: "View  _MENU_",
 				paginate: {
@@ -58,7 +55,7 @@ $(function() {
 			targetDt.row('.selected').remove().draw( false );
 			return false;
 		});
-		$("div.contact-toolbar-left").html('<div class="d-xxl-flex d-none align-items-center"> <select class="form-select form-select-sm w-120p"><option selected>Bulk actions</option><option value="1">Edit</option><option value="2">Move to trash</option></select> <button class="btn btn-sm btn-light ms-2">Apply</button></div><div class="d-xxl-flex d-none align-items-center form-group mb-0"> <label class="flex-shrink-0 mb-0 me-2">Sort by:</label> <select class="form-select form-select-sm w-130p"><option selected>Date Created</option><option value="1">Date Edited</option><option value="2">Frequent Contacts</option><option value="3">Recently Added</option> </select></div> <select class="d-flex align-items-center w-130p form-select form-select-sm"><option selected>Export to CSV</option><option value="2">Export to PDF</option><option value="3">Send Message</option><option value="4">Delegate Access</option> </select>');
+		// $("div.contact-toolbar-left").html('<div class="d-xxl-flex d-none align-items-center"> <select class="form-select form-select-sm w-120p"><option selected>Bulk actions</option><option value="1">Edit</option><option value="2">Move to trash</option></select> <button class="btn btn-sm btn-light ms-2">Apply</button></div><div class="d-xxl-flex d-none align-items-center form-group mb-0"> <label class="flex-shrink-0 mb-0 me-2">Sort by:</label> <select class="form-select form-select-sm w-130p"><option selected>Date Created</option><option value="1">Date Edited</option><option value="2">Frequent Contacts</option><option value="3">Recently Added</option> </select></div> <select class="d-flex align-items-center w-130p form-select form-select-sm"><option selected>Export to CSV</option><option value="2">Export to PDF</option><option value="3">Send Message</option><option value="4">Delegate Access</option> </select>');
 		$("#datable_1").parent().addClass('table-responsive');
 		
 		/*Select all using checkbox*/
